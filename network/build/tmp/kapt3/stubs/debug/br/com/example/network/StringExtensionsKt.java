9118afe391356908dda34322ef6c60c8a163fc16
Luiz Filho <luiizfiilhoo@gmail.com>
2021-10-26 10:33:43 -0400

    Removing unecessary things

--- network/build/tmp/kapt3/stubs/debug/br/com/example/network/StringExtensionsKt.java
+++
@@ -1,7 +0,0 @@
-package br.com.example.network;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 5, 1}, k = 2, d1 = {"\u0000\u0016\n\u0002\b\u0002\n\u0002\u0010\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u001a0\u0010\u0000\u001a\u0004\u0018\u0001H\u0001\"\n\b\u0000\u0010\u0001\u0018\u0001*\u00020\u0002*\u00020\u00032\u000e\u0010\u0004\u001a\n\u0012\u0006\b\u0001\u0012\u0002H\u00010\u0005H\u0086\b\u00a2\u0006\u0002\u0010\u0006\u00a8\u0006\u0007"}, d2 = {"toJsonObject", "T", "", "", "clazz", "Lkotlin/reflect/KClass;", "(Ljava/lang/String;Lkotlin/reflect/KClass;)Ljava/lang/Object;", "network_debug"})
-public final class StringExtensionsKt {
-}
